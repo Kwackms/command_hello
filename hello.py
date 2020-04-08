74193028649a2c553f0712022bd08ab87f88177e
kwakms <63217979+Kwackms@users.noreply.github.com>
2020-04-08 09:45:05 +0900

    GitHub for  git pull

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // For command line git tutorial
 // Local repository modification
 // GitHub modified
@@ -7,3 +6,4 @@ print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
